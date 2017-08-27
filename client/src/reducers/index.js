4fc3c1247942bc5824ac6176c038f167fe152db5
Anshul Saxena <anshul.swarup@gmail.com>
2017-08-27 23:33:28 +0530

    Final version of of Full Stack React App

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm //we assign reduxForm reducer to key of form
+  form: reduxForm, //we assign reduxForm reducer to key of form
+  surveys: surveysReducer
 });
